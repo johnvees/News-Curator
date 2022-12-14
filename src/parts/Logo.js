b741bcaf498b59ef9e26ce9c5c4e31cd578bb09f
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-12-14 14:32:21 +0700

    update color variable

---
+++ src/parts/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Button from '../elements/Button';
+
+export default function Logo() {
+  return (
+    <Button className="navbar-logo" href="#" type="link">
+      <img src="../assets/images/Logo Project.png" alt="Logo" />
+    </Button>
+  );
+}
