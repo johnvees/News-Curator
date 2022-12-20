b8038d20945adf8f612762f19cc7e8685a16cc4f
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-12-20 07:41:05 +0700

    update button masuk styling

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -11,50 +11,6 @@ export default function Header(props) {
 
   return (
     <header className="spacing-sm">
-      {/* <Navbar className="navbar-expand-lg navbar-light">
-        <div className="container-fluid">
-          <ImgLogo />
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-          >
-            <Nav className="navbar-nav mr-auto">
-              <Nav.Link className={`nav-item${getNavLinkClass('/')}`}>
-                <Button className="nav-link ml-5" type="link" href="/">
-                  Beranda
-                </Button>
-              </Nav.Link>
-              <Nav.Link
-                className={`nav-item dropdown${getNavLinkClass('/dropdown')}`}
-              >
-                <NavDropdown title="Pencarian" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">
-                    Portal Berita
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Dokumen
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    URL / Link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav.Link>
-              <Nav.Link className={`nav-item${getNavLinkClass('/tentang')}`}>
-                <Button className="nav-link" type="link" href="/tentang">
-                  Tentang
-                </Button>
-              </Nav.Link>
-            </Nav>
-            <ul className="navbar-nav ml-auto">
-              <Nav.Link className={`nav-item${getNavLinkClass('/masuk')}`}>
-                <Button className="nav-link mr-5" type="link" href="/masuk">
-                  Masuk / Daftar
-                </Button>
-              </Nav.Link>
-            </ul>
-          </Navbar.Collapse>
-        </div>
-      </Navbar> */}
       <div className="container-fluid">
         <Navbar bg="light" expand="lg">
           <ImgLogo />
@@ -85,7 +41,11 @@ export default function Header(props) {
             </ul>
             <ul className="navbar-nav ml-auto">
               <li className={`nav-item${getNavLinkClass('/masuk')}`}>
-                <Button className="nav-link mr-5" type="link" href="/masuk">
+                <Button
+                  className="btn btn-outline-primary mr-5"
+                  type="link"
+                  href="/masuk"
+                >
                   Masuk / Daftar
                 </Button>
               </li>
